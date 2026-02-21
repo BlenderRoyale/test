efea50d23222b83bf95c6e24d60f72ca7016ac5e
BlenderRoyale <106525051+BlenderRoyale@users.noreply.github.com>
2026-02-21 19:35:10 +0000

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import BubbleContactApp from "./bubble_contact.js";
-import { createWebSDK } from "./web_sdk.js";
-
-const sdk = createWebSDK({ rootId: "root" });
-const app = new BubbleContactApp(sdk);
-
-app.open();
